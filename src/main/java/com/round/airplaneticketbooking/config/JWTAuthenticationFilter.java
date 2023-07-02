f9db0c0c502e4e2cb76a1a7c4528c5fe173f06eb
Pranav Jothimani <pranav.2001146@srec.ac.in>
2023-07-02 18:25:35 +0530

    Update JWTAuthenticationFilter.java

--- src/main/java/com/round/airplaneticketbooking/config/JWTAuthenticationFilter.java
+++ src/main/java/com/round/airplaneticketbooking/config/JWTAuthenticationFilter.java
@@ -1,14 +1,17 @@
 package com.round.airplaneticketbooking.config;
 
-import com.round.airplaneticketbooking.util.JwtTokenProvider;
+import com.round.airplaneticketbooking.util.AdminDetailsService;
+import com.round.airplaneticketbooking.util.CustomerDetailsService;
+import com.round.airplaneticketbooking.util.JwtService;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.lang.NonNull;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
@@ -16,67 +19,58 @@ import org.springframework.web.filter.OncePerRequestFilter;
 import java.io.IOException;
 
 @Component
+@RequiredArgsConstructor
 public class JWTAuthenticationFilter extends OncePerRequestFilter {
 
-    private final JwtTokenProvider jwtTokenProvider;
-    private final UserDetailsService adminDetailsService;
-    private final UserDetailsService customerDetailsService;
+    private final JwtService jwtService;
+    private final AdminDetailsService adminDetailsService;
+    private final CustomerDetailsService customerDetailsService;
 
-    public JWTAuthenticationFilter(JwtTokenProvider jwtTokenProvider,
-                                   UserDetailsService adminDetailsService,
-                                   UserDetailsService customerDetailsService) {
-        this.jwtTokenProvider = jwtTokenProvider;
-        this.adminDetailsService = adminDetailsService;
-        this.customerDetailsService = customerDetailsService;
-    }
 
     @Override
-    protected void doFilterInternal(HttpServletRequest request,
-                                    HttpServletResponse response,
-                                    FilterChain filterChain) throws ServletException, IOException {
+    protected void doFilterInternal(
+            @NonNull HttpServletRequest request,
+            @NonNull HttpServletResponse response,
+            @NonNull FilterChain filterChain
+    ) throws ServletException, IOException {
         final String authorizationHeader = request.getHeader("Authorization");
         final String jwtToken;
-        Long userId = null;
+        String userEmail = null;
 
+        // If the Authorization header is missing or does not start with "Bearer ", proceed to the next filter
         if (authorizationHeader == null || !authorizationHeader.startsWith("Bearer ")) {
-            // If the Authorization header is missing or does not start with "Bearer ", proceed to the next filter
             filterChain.doFilter(request, response);
             return;
         }
 
         jwtToken = authorizationHeader.substring(7);
+        userEmail = jwtService.extractUsername(jwtToken);
         String userType = getUserTypeFromRequest(request);
 
         if (userType != null && SecurityContextHolder.getContext().getAuthentication() == null) {
             UserDetails userDetails;
-            UserDetailsService userDetailsService;
 
             if (userType.equals("admin")) {
-                userId = jwtTokenProvider.getUserIdFromToken(jwtToken);
-                userDetails = adminDetailsService.loadUserByUsername(userId.toString());
-                userDetailsService = adminDetailsService;
+                userDetails = adminDetailsService.loadUserByUsername(userEmail);
             } else if (userType.equals("customer")) {
-                userId = jwtTokenProvider.getUserIdFromToken(jwtToken);
-                userDetails = customerDetailsService.loadUserByUsername(userId.toString());
-                userDetailsService = customerDetailsService;
+                userDetails = customerDetailsService.loadUserByUsername(userEmail);
             } else {
                 // Unknown user type
                 filterChain.doFilter(request, response);
                 return;
             }
 
-            if (jwtTokenProvider.validateToken(jwtToken, userId)) {
+            if (jwtService.isTokenValid(jwtToken, userDetails)) {
                 // If the JWT token is valid, create an authentication token and set it in the security context
-                UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(
-                        userDetails, null, userDetails.getAuthorities()
+                UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(
+                        userDetails,
+                        null,
+                        userDetails.getAuthorities()
                 );
-                authenticationToken.setDetails(
+                authToken.setDetails(
                         new WebAuthenticationDetailsSource().buildDetails(request)
                 );
-                SecurityContextHolder.getContext().setAuthentication(authenticationToken);
-            } else {
-                // Invalid token
-                SecurityContextHolder.clearContext();
+                SecurityContextHolder.getContext().setAuthentication(authToken);
             }
         }
 
@@ -85,7 +79,6 @@ public class JWTAuthenticationFilter extends OncePerRequestFilter {
 
     private String getUserTypeFromRequest(HttpServletRequest request) {
         // Retrieve the user type from the request URL or headers
-        // You can modify this code according to your application's URL structure or header conventions
         String requestURI = request.getRequestURI();
         if (requestURI.contains("/admin/")) {
             return "admin";
